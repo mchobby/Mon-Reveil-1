209a0e47e3afd39d83eea5f93f1f7d06fab73604
Stefan Gotkowski <stefan.gotkowski@gmail.com>
2018-03-12 15:23:21 +0100

    Update melodies.h

--- Mon-Reveil/melodies.h
+++ Mon-Reveil/melodies.h
@@ -1,35 +1,35 @@
 // Axel F mélodie
 // Source https://github.com/nseidle/AxelF_DoorBell/tree/master/AxelF
-#define AXEL_F { \
-  {NOTE_D4,  8}, \
-  {0,        8}, \
-  {NOTE_F4,  6}, \
-  {NOTE_D4,  16},\
-  {0,        16},\
-  {NOTE_D4,  16},\
-  {NOTE_G4,  8}, \
-  {NOTE_D4,  8}, \
-  {NOTE_C4,  8}, \
-  {NOTE_D4,  8}, \
-  {0,        8}, \
-  {NOTE_A4,  6}, \
-  {NOTE_D4,  16},\
-  {0,        16},\
-  {NOTE_D4,  16},\
-  {NOTE_AS4, 8}, \
-  {NOTE_A4,  8}, \
-  {NOTE_F4,  8}, \
-  {NOTE_D4,  8}, \
-  {NOTE_A4,  8}, \
-  {NOTE_D5,  8}, \
-  {NOTE_D4,  16},\
-  {NOTE_C4,  16},\
-  {0,        16},\
-  {NOTE_C4,  16},\
-  {NOTE_A3,  8}, \
-  {NOTE_E4,  8}, \
-  {NOTE_D4,  2}, \
-  {NOTE_D4,  2}, \
+#define AXEL_F {  \
+  {NOTE_D4,  8},  \
+  {0,        8},  \
+  {NOTE_F4,  6},  \
+  {NOTE_D4,  16}, \
+  {0,        16}, \
+  {NOTE_D4,  16}, \
+  {NOTE_G4,  8},  \
+  {NOTE_D4,  8},  \
+  {NOTE_C4,  8},  \
+  {NOTE_D4,  8},  \
+  {0,        8},  \
+  {NOTE_A4,  6},  \
+  {NOTE_D4,  16}, \
+  {0,        16}, \
+  {NOTE_D4,  16}, \
+  {NOTE_AS4, 8},  \
+  {NOTE_A4,  8},  \
+  {NOTE_F4,  8},  \
+  {NOTE_D4,  8},  \
+  {NOTE_A4,  8},  \
+  {NOTE_D5,  8},  \
+  {NOTE_D4,  16}, \
+  {NOTE_C4,  16}, \
+  {0,        16}, \
+  {NOTE_C4,  16}, \
+  {NOTE_A3,  8},  \
+  {NOTE_E4,  8},  \
+  {NOTE_D4,  2},  \
+  {NOTE_D4,  2},  \
   {0,        2}}
 
 // Mario mélodie
@@ -57,14 +57,14 @@
   {NOTE_G6, 12}, \
   {0,       12}, \
   {0,       12}, \
-  {NOTE_E6, 12}, \ 
+  {NOTE_E6, 12}, \
   {0,       12}, \
   {0,       9},  \
   {NOTE_A6, 9},  \
   {0,       9},  \
   {NOTE_B6, 12}, \
-  {0,       12}, \ 
-  {NOTE_AS6,12}, \ 
+  {0,       12}, \
+  {NOTE_AS6,12}, \
   {NOTE_A6, 12}, \
   {0,       12}, \
   {NOTE_G6, 12}, \
@@ -77,7 +77,7 @@
   {0,       12}, \
   {NOTE_E7, 12}, \
   {0,       12}, \
-  {NOTE_C7, 12}, \ 
+  {NOTE_C7, 12}, \
   {NOTE_D7, 12}, \
   {NOTE_B6, 12}, \
   {0,       12}, \
@@ -95,7 +95,7 @@
   {0,       12}, \
   {NOTE_B6, 12}, \
   {0,       12}, \
-  {NOTE_AS6,12}, \ 
+  {NOTE_AS6,12}, \
   {NOTE_A6, 12}, \
   {0,       12}, \
   {NOTE_G6, 9},  \
@@ -131,28 +131,28 @@
   {NOTE_E5,  10}, \
   {NOTE_F5,  7},  \
   {NOTE_C5,  3},  \
-  {NOTE_GS4, 10}, \ 
+  {NOTE_GS4, 10}, \
   {NOTE_F4,  7},  \
   {NOTE_C5,  3},  \
-  {NOTE_A4,  20}, \ 
+  {NOTE_A4,  20}, \
   {NOTE_A5,  10}, \
   {NOTE_A4,  7},  \
   {NOTE_A4,  3},  \
   {NOTE_A5,  10}, \
-  {NOTE_GS5, 5},  \ 
+  {NOTE_GS5, 5},  \
   {NOTE_G5,  5},  \
-  {NOTE_FS5, 2},  \ 
+  {NOTE_FS5, 2},  \
   {NOTE_F5,  2},  \
-  {NOTE_FS5, 5},  \ 
-  {NOTE_AS4, 5},  \ 
-  {NOTE_DS5, 10}, \ 
+  {NOTE_FS5, 5},  \
+  {NOTE_AS4, 5},  \
+  {NOTE_DS5, 10}, \
   {NOTE_D5,  5},  \
-  {NOTE_CS5, 5},  \ 
+  {NOTE_CS5, 5},  \
   {NOTE_C5,  2},  \
-  {NOTE_AS4, 2},  \ 
+  {NOTE_AS4, 2},  \
   {NOTE_C5,  5},  \
   {NOTE_F4,  2},  \
-  {NOTE_GS4, 10}, \ 
+  {NOTE_GS4, 10}, \
   {NOTE_F4,  7},  \
   {NOTE_A4,  2},  \
   {NOTE_C5,  10}, \
@@ -163,20 +163,20 @@
   {NOTE_A4,  7},  \
   {NOTE_A4,  3},  \
   {NOTE_A5,  10}, \
-  {NOTE_GS5, 5},  \ 
+  {NOTE_GS5, 5},  \
   {NOTE_G5,  5},  \
   {NOTE_FS5, 2},  \
   {NOTE_F5,  2},  \
-  {NOTE_FS5, 5},  \ 
-  {NOTE_AS4, 5},  \ 
-  {NOTE_DS5, 10}, \ 
+  {NOTE_FS5, 5},  \
+  {NOTE_AS4, 5},  \
+  {NOTE_DS5, 10}, \
   {NOTE_D5,  5},  \
-  {NOTE_CS5, 5},  \ 
+  {NOTE_CS5, 5},  \
   {NOTE_C5,  2},  \
-  {NOTE_AS4, 2},  \ 
+  {NOTE_AS4, 2},  \
   {NOTE_C5,  5},  \
   {NOTE_F4,  5},  \
-  {NOTE_GS4, 10}, \ 
+  {NOTE_GS4, 10}, \
   {NOTE_F4,  7},  \
   {NOTE_C5,  3},  \
   {NOTE_A4,  10}, \
@@ -188,4 +188,3 @@
 #define ALARM1{  \
   {NOTE_A4,  20},\
   {NOTE_C4,  20}}
-

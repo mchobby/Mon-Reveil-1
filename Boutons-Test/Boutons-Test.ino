9d4367fc9513dc381effa1cf1912e42f3ef6de84
Stefan Gotkowski <stefan.gotkowski@gmail.com>
2018-03-23 10:02:39 +0100

    Update Boutons-Test.ino

--- Boutons-Test/Boutons-Test.ino
+++ Boutons-Test/Boutons-Test.ino
@@ -9,19 +9,19 @@
 * 
 * 
 */
-#define boutonLuminosite    2  // digital
-#define boutonAlarmeControl 3  // digital
-#define boutonPlus          9  // digital
-#define boutonMoins         10 // digital
-#define piezoBuzzer         13 // digital
-#define boutonOk            14 // analog 0
-#define boutonSnooze        15 // analog 1
+#define BOUTON_LUMINOSITE    2  // digital
+#define BOUTON_ALARME_CONTROL 3  // digital
+#define BOUTON_PLUS          9  // digital
+#define BOUTON_MOINS         10 // digital
+#define PIEZO_BUZZER         13 // digital
+#define BOUTON_OK            14 // analog 0
+#define BOUTON_SNOOZE        15 // analog 1
 
-#define ledBoutonOK         16 // analog 2
-#define ledBoutonSnooze     17 // analog 3
+#define LED_BOUTON_OK         16 // analog 2
+#define LED_BOUTON_SNOOZE     17 // analog 3
 
 
-const int BOUTON_ALARME[] = {8, 7, 9, 10};  // Quelles pin pour chaques alarmes
+const int BOUTONALARME[] = {8, 7, 5, 4};  // Quelles pin pour chaques alarmes
 const int NBRALARMES = sizeof( BOUTONALARME ) / sizeof( int );  // Combien d'alarmes
 
 void setup() {
@@ -29,40 +29,40 @@ void setup() {
   Serial.begin( 9600 );
   
   // Initialisation des boutons
-  pinMode( boutonOk, INPUT_PULLUP );
-  pinMode( boutonMoins, INPUT_PULLUP );
-  pinMode( boutonPlus, INPUT_PULLUP );
-  pinMode( boutonLuminosite, INPUT_PULLUP );
-  pinMode( boutonAlarmeControl, INPUT_PULLUP );
-  pinMode( boutonSnooze, INPUT_PULLUP );
+  pinMode( BOUTON_OK, INPUT_PULLUP );
+  pinMode( BOUTON_MOINS, INPUT_PULLUP );
+  pinMode( BOUTON_PLUS, INPUT_PULLUP );
+  pinMode( BOUTON_LUMINOSITE, INPUT_PULLUP );
+  pinMode( BOUTON_ALARME_CONTROL, INPUT_PULLUP );
+  pinMode( BOUTON_SNOOZE, INPUT_PULLUP );
   for(int i=0 ; i<NBRALARMES ;i++)
     pinMode( BOUTONALARME[i], INPUT_PULLUP );
 
   // Initialisation des leds et du piezo
-  pinMode( ledBoutonOK, OUTPUT );
-  pinMode( ledBoutonSnooze, OUTPUT );
-  pinMode( piezoBuzzer, OUTPUT );
+  pinMode( LED_BOUTON_OK, OUTPUT );
+  pinMode( LED_BOUTON_SNOOZE, OUTPUT );
+  pinMode( PIEZO_BUZZER, OUTPUT );
 }
 
 void loop(){
   // Test bouton ok
-  Serial.println("Appuyez sur le bouton 'OK'");
-  while(!estAppuye(boutonOk));
+  Serial.println("Appuyiez sur le bouton 'OK'");
+  while(!estAppuye(BOUTON_OK));
   // Test bouton snooze
   Serial.println("Appuyez sur le bouton 'SNOOZE'");
-  while(!estAppuye(boutonSnooze));
+  while(!estAppuye(BOUTON_SNOOZE));
   // Test bouton plus
   Serial.println("Appuyez sur le bouton 'Plus'");
-  while(!estAppuye(boutonPlus));
+  while(!estAppuye(BOUTON_PLUS));
   // Test bouton moins
   Serial.println("Appuyez sur le bouton 'Moins'");
-  while(!estAppuye(boutonMoins));
+  while(!estAppuye(BOUTON_MOINS));
   // Test bouton luminosité
   Serial.println("Appuyez sur le bouton 'Luminosité'");
-  while(!estAppuye(boutonLuminosite));
+  while(!estAppuye(BOUTON_LUMINOSITE));
   // Test bouton alarme contrôle
   Serial.println("Appuyez sur le bouton 'Alarmes Contrôles'");
-  while(!estAppuye(boutonAlarmeControl));
+  while(!estAppuye(BOUTON_ALARME_CONTROL));
   // Test bouton activer alarmes
   for(int i=0; i<NBRALARMES ; i++){
     Serial.print("Appuyez sur le bouton 'Alarmes Actiavation ");
@@ -72,19 +72,19 @@ void loop(){
   }
   // Test led ok
   Serial.println("Appuyez sur le bouton 'Ok', si vous voyez la led du bouton ok allumée");
-  digitalWrite(ledBoutonOK ,HIGH);
-  while(!estAppuye(boutonOk));
-  digitalWrite(ledBoutonOK ,LOW);
+  digitalWrite(LED_BOUTON_OK ,HIGH);
+  while(!estAppuye(BOUTON_OK));
+  digitalWrite(LED_BOUTON_OK ,LOW);
   // Test led Snooze
   Serial.println("Appuyez sur le bouton 'Snooze', si vous voyez la led du bouton snooze allumée");
-  digitalWrite(ledBoutonSnooze ,HIGH);
-  while(!estAppuye(boutonSnooze));
-  digitalWrite(ledBoutonSnooze ,LOW);
+  digitalWrite(LED_BOUTON_SNOOZE ,HIGH);
+  while(!estAppuye(BOUTON_SNOOZE));
+  digitalWrite(LED_BOUTON_SNOOZE ,LOW);
   // Test piezzo buzzer
   Serial.println("Entendez-vous le piezo sonner ?");
-  tone(piezoBuzzer, 1000);
-  while(!estAppuye(boutonOk));
-  noTone(piezoBuzzer);
+  tone(PIEZO_BUZZER, 1000);
+  while(!estAppuye(BOUTON_OK));
+  noTone(PIEZO_BUZZER);
   Serial.println("Tout est bon !!!");
   delay(1000);
   return;
